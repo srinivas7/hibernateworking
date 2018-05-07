4703f3326b2e2b7502371245ca96741cbf2e8939
srinivas7 <vasu4727@gmail.com>
2018-05-07 20:20:25 +0530

    trying authentication

---
+++ src/main/java/com/example/model/security/AuthorityName.java
@@ -0,0 +1,5 @@
+package com.example.model.security;
+
+public enum AuthorityName {
+    ROLE_USER, ROLE_ADMIN
+}
---
+++ src/main/java/com/example/security/controller/AuthenticationException.java
@@ -0,0 +1,7 @@
+package com.example.security.controller;
+
+public class AuthenticationException extends RuntimeException {
+    public AuthenticationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
